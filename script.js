2f3ebe74d7ee16475827448383ae7aeee1704dd6
SanyaREZYA <alex07082005m@gmail.com>
2024-05-08 17:02:42 +0300

    wwq

--- script.js
+++ script.js
@@ -14,9 +14,27 @@ document.addEventListener("DOMContentLoaded", function() {
     function changeProfession(index) {
         professionName.textContent = professionDescriptions[index];
         for (let i = 0; i < professionImages.length; i++) {
+            if (index === 2) {
+                switch (i) {
+                    case 0:
+                        professionImages[i].setAttribute('onclick', 'window.location.href="laba1.html"');
+                        break;
+                    case 1:
+                        professionImages[i].setAttribute('onclick', 'window.location.href="laba2.html"');
+                        break;
+                    case 2:
+                        professionImages[i].setAttribute('onclick', 'window.location.href="laba3.html"');
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                professionImages[i].removeAttribute('onclick');
+            }
             professionImages[i].src = `images/image_${index}_${i + 1}.jpg`;
         }
     }
+    
 
     function nextProfession() {
         currentIndex = (currentIndex + 1) % professionDescriptions.length;
